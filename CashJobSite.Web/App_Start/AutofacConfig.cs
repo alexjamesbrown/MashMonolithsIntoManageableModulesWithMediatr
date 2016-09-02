89b0a1c93248e90524229cc2048f7eb1c8300e52
Alex Brown <alex@cohoda.com>
2016-09-02 21:11:21 +0100

    removed unused usings

--- CashJobSite.Web/App_Start/AutofacConfig.cs
+++ CashJobSite.Web/App_Start/AutofacConfig.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using Autofac;
 using Autofac.Integration.Mvc;
 using CashJobSite.Application;
