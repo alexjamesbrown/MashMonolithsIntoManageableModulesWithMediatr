a238f47e152d7bf6e0b21de49cf5cb26dc1ff559
Alex Brown <alex@cohoda.com>
2016-09-02 11:48:14 +0100

    refactored AddJobApplication into query / handler removed dependency on IJobService in JobController - no longer needed

---
+++ CashJobSite.Application/CommandHandlers/AddJobApplicationCommandHandler.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Linq;
+using CashJobSite.Application.Commands;
+using CashJobSite.Application.Logging;
+using CashJobSite.Data;
+using CashJobSite.Models;
+using MediatR;
+
+namespace CashJobSite.Application.CommandHandlers
+{
+    public class AddJobApplicationCommandHandler : IRequestHandler<AddJobApplicationCommand, Unit>
+    {
+        private readonly ILogger _logger;
+        private readonly IEmailService _emailService;
+        private readonly ICashJobSiteDbContext _dbContext;
+
+        public AddJobApplicationCommandHandler(ILogger logger, IEmailService emailService, ICashJobSiteDbContext dbContext)
+        {
+            _logger = logger;
+            _emailService = emailService;
+            _dbContext = dbContext;
+        }
+
+        public Unit Handle(AddJobApplicationCommand message)
+        {
+            //again, could use mediator here to call back to our GetJobById query
+            var job = _dbContext.Jobs.SingleOrDefault(x => x.Id == message.JobId);
+
+            var jobApplication = new JobApplication
+            {
+                CandidateName = message.CandidateName,
+                CandidateEmail = message.CandidateEmail,
+                CandidateInfo = message.CandidateInfo,
+                Job = job
+            };
+
+            _dbContext.JobApplications.Add(jobApplication);
+            _dbContext.SaveChanges();
+
+            var emailSubject = "Job application received.";
+            var emailBody = "You have a new application for your job #" + job.Id + "\n" +
+                            "Name: " + message.CandidateName + "\n" +
+                            "Email: " + message.CandidateEmail + "\n" +
+                            "Info: " + message.CandidateInfo + "\n";
+
+            _emailService.SendEmail(job.BossEmail, emailSubject, emailBody);
+            _logger.Debug("Email Sent");
+
+            return Unit.Value;
+        }
+    }
+}
---
+++ CashJobSite.Application/Commands/AddJobApplicationCommand.cs
@@ -0,0 +1,23 @@
+﻿using MediatR;
+
+namespace CashJobSite.Application.Commands
+{
+    public class AddJobApplicationCommand : IRequest<Unit>
+    {
+        public AddJobApplicationCommand(int jobId, string candidateName, string candidateEmail, string candidateInfo)
+        {
+            JobId = jobId;
+            CandidateName = candidateName;
+            CandidateEmail = candidateEmail;
+            CandidateInfo = candidateInfo;
+        }
+
+        public int JobId { get; set; }
+
+        public string CandidateName { get; set; }
+
+        public string CandidateEmail { get; set; }
+
+        public string CandidateInfo { get; set; }
+    }
+}
